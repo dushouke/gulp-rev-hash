9991d06745d0e31b42f1ff72cedd6364c4adf59d
Matteo Nardini <matteo.shalen@gmail.com>
2017-04-19 22:58:38 +0200

    Added jshint and removed unused code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+"use strict";
+
 var path = require('path');
 var fs = require('fs');
-var EOL = require('os').EOL;
 
 var through = require('through2');
 var gutil = require('gulp-util');
@@ -12,14 +13,14 @@ module.exports = function(options) {
     var endReg = /<!--\s*end\s*-->/gim;
     var jsAndCssReg = /<\s*script\s+.*?src\s*=\s*"([^"]+?)\.js.*".*?><\s*\/\s*script\s*>|<\s*link\s+.*?href\s*=\s*"([^"]+?)\.css.*".*?>/gi;
     var regSpecialsReg = /([.?*+^$[\]\\(){}|-])/g;
-    var basePath, mainPath, mainName, alternatePath;
+    var basePath, mainPath, mainName;
 
     function getTags(content) {
         var tags = [];
 
-        content
-            .replace(/<!--(?:(?:.|\r|\n)*?)-->/gim, '')
-            .replace(jsAndCssReg, function(a, b, c) {
+        const replF = function(a, b, c) {
+                console.log("a: " + a + "  b: " + b + "  c: " + c);
+
                 var path = b || c,
                     pathParts = path.split('-v'),
                     extension = b ? '.js' : '.css';
@@ -31,24 +32,19 @@ module.exports = function(options) {
                     extension: b ? '.js' : '.css',
                     pathReg: new RegExp(escapeRegSpecials(path + extension), 'g')
                 });
-            });
+            };
+
+        content
+            .replace(/<!--(?:(?:.|\r|\n)*?)-->/gim, '')
+            .replace(jsAndCssReg, replF);
+
+
         //console.log(tags);
         return tags;
     }
 
     function escapeRegSpecials(str) {
         return (str + '').replace(regSpecialsReg, "\\$1");
-    }
-
-    function endsWith(subjectString, searchString, position) {
-        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
-            position = subjectString.length;
-        }
-        position -= searchString.length;
-
-        var lastIndex = subjectString.lastIndexOf(searchString, position);
-
-        return lastIndex !== -1 && lastIndex === position;
     }
 
     return through.obj(function(file, enc, callback) {
@@ -72,7 +68,7 @@ module.exports = function(options) {
                     var section = sections[i].split(startReg);
                     var tags = getTags(section[1]);
                     html.push(section[0]);
-                    html.push('<!-- rev-hash -->\r\n')
+                    html.push('<!-- rev-hash -->\r\n');
 
                     for (var j = 0; j < tags.length; j++) {
                         tag = tags[j];
